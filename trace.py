e51620626e8a08845bd00c8fa8a2b0a4c8b519ac
Alan <afedoruk@mtroyal.ca>
2025-10-09 12:08:23 -0600

    Add files via upload

---
+++ trace.py
@@ -0,0 +1,33 @@
+#
+# tracing example 
+#
+
+def f( a:int) -> str:
+    """ categorizes numnbers """
+    category = ""
+    if a < 0:
+        category = "negative"
+    if a == 0: 
+        category = "zero"
+    else:
+        category = "positive"
+    return category
+
+def g( x:int, a:str) -> bool:
+    """ Returns a boolean """
+    b = False
+    if (x < 0 and a == "negative") or (x > 0 and a == "positive"):
+        b = True
+    return b
+
+def main() -> None:
+    a = int(input( "enter a number "))
+    x = int(input( "enter another number"))
+
+    cat = f(a)
+    if g(x,cat):
+        print("We did it!")
+    else:
+        print("We didnt do it")
+    
+main()
