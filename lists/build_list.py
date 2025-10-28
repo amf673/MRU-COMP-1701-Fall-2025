96992a7c7f8eda533fee8f96a36a1256f3c0a987
Alan <afedoruk@mtroyal.ca>
2025-10-28 11:39:45 -0600

    Create build_list.py

---
+++ lists/build_list.py
@@ -0,0 +1,12 @@
+#
+# 
+# building a list from input  
+#
+
+alist = []
+item = input( "Enter and item: ")
+while item != "":
+    alist.append(item)
+    item = input( "Enter and item: ")
+
+print(alist)
