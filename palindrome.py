e38f91b8f1114ce5e4a4c909240e7cde9aeca9ff
Alan <afedoruk@mtroyal.ca>
2025-11-12 15:17:23 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -14,23 +14,26 @@ def reverse( a_str:str) -> str:
     return new_str
 
 def palindrome( word:str) -> bool: 
-    """ Returns true if a_str is a palindrome """
+    """ Returns true if word is a palindrome """
     
-    palin_flag = True
+    is_palindrome = True # assume it is a palindrome
     i = 0 
-    while i < (len(word) // 2) - 1 and palin_flag:
+    # Only need to compare the first half to the second
+    # if len = 1 or 0 then (len(word) // 2) = 0 and the loop will not execute 
+    # and is_palindrome will remain True
+    while i < (len(word) // 2) - 1 and is_palindrome:
         if word[i] != word[-1 * i]:
-            palin_flag = False
+            is_palindrome = False
         i = i + 1
-    return palin_flag
+    return is_palindrome
 
 def palindrome2( word:str) -> bool: 
-    """ Returns true if a_str is a palindrome """
+    """ Returns true if word is a palindrome """
     
     return reverse( word) == word
 
 def palindrome3( word:str) -> bool: 
-    """ Returns true if a_str is a palindrome using recursion """
+    """ Returns true if word is a palindrome using recursion """
     
     if len(word) < 2:  
         # a word of length 1 or 0 is a palindrome
