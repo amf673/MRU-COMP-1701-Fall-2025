cacfeb895122be592e5a85217b6dfe183d35d536
Alan <afedoruk@mtroyal.ca>
2025-11-25 10:46:44 -0700

    Create table.py

---
+++ assign3/table.py
@@ -0,0 +1,13 @@
+# A sample to show a table constructed by a list of lists. 
+# 
+table = [
+    ["id",    "name",          "program",                       "gpa"],
+    ["201345","Ford",          "BSc Data Science",              "3.6"],
+    ["201781","Zaphod",        "B Computer Information Systems","3.5"],
+    ["201999","Trillian",      "BSc Computer Science",          "4.0"],
+    ["102000","Slartibartfast","BSc Geology",                   "2.8"],
+    ["201678","Arthur",        "B Computer Information Systems","3.1"]
+]
+
+print( table)
+
